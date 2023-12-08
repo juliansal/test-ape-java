77d562d0332a097f86797d911124bfc9445a9e25
Julian Salguero <juliansal.slc@gmail.com>
2023-12-07 21:24:32 -0800

    remove unused import

--- testcase-manager/src/test/java/io/testcasemanager/TestApeAppTest.java
+++ testcase-manager/src/test/java/io/testcasemanager/TestApeAppTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @DisplayName("Test Ape Application Context")
